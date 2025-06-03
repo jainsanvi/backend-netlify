defdca2aeea4eff3715412423f759d5b2eab620b
Jain Sanvi <163547915+jainsanvi@users.noreply.github.com>
2025-06-03 15:37:55 +0530

    second

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const serverless = require('serverless-http')
+
+const app = express();
+
+app.use(cors())
+
+
+app.get('/.netlify/functions/index/',(req, res) => {
+    res.send("success");
+})
+
+app.use(express.json())
+
+// app.listen(5000, () => {
+//     console.log("Server is running on the port");
+// })
+
+module.exports.handlers = serverless(app);
